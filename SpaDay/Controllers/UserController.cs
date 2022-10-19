83216838f1586658f6b755f3fba464f0a22c3e07
Bethany <morphforlife@gmail.com>
2022-10-19 18:30:54 -0500

    From last week

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+public class User
+{
+	public User()
+	{
+		[HttpGet]
+		[Route("/user/add")]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[Route("/user/add")]
+		public IActionResult SubmitAddUserForm(User newUser, string verify)
+		{
+            UserData.Add(newUser, verify);
+
+
+            return Redirect("/user");
+        }
+	}
+}
